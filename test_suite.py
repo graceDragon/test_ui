11c795b40c750b942261e03523105c2e5a246143
liuyl <yulianguil@163.com>
2018-03-29 15:51:11 +0800

    优化case

--- test_suite.py
+++ test_suite.py
@@ -6,10 +6,10 @@ from zhiyu_app.test_case import case01_login as zhiyu_login
 suite = unittest.TestSuite()
 
 # 运行单个case
-suite.addTest(youli_login.LogIn('test_login'))
+# suite.addTest(youli_login.LogIn('test_login'))
 # suite.addTest(zhiyu_login.LogIn('test_login'))
 
 # 运行一组case
-# suite.addTest(unittest.makeSuite(youli_login.LogIn))
-# suite.addTest(unittest.makeSuite(zhiyu_login.LogIn))
+suite.addTest(unittest.makeSuite(youli_login.LogIn))
+suite.addTest(unittest.makeSuite(zhiyu_login.LogIn))
 
